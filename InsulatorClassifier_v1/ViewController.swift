86c14e80c2a3894602118b8b4e33f2cdd305484d
Yehia <yehia.fahmy@uwaterloo.ca>
2021-03-30 12:20:42 -0400

    added functionality to take or choose a photo, then display this photo in the center of the screen 

--- InsulatorClassifier_v1/ViewController.swift
+++ InsulatorClassifier_v1/ViewController.swift
@@ -7,17 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    @IBOutlet weak var centerImageView: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
+    // create the object
+    let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Insulator Classifier"
+        // image picker initialization
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .camera
     }
 
     @IBAction func cameraButtonTapped(_ sender: UIBarButtonItem) {
-        print("camera button tapped")
+        // change the image source first
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func photoButtonTapped(_ sender: UIBarButtonItem) {
+        // change the image source first
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // if an image was picked
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
     }
     
 }
