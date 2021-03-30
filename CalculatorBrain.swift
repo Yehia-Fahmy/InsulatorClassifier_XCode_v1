6f7c5703567b97fc1819ac5b863aac8d023d1fd6
Yehia <yehia.fahmy@uwaterloo.ca>
2021-03-30 11:27:39 -0400

    redesigning the main storyboard to be much more simple

---
+++ CalculatorBrain.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Yehia Fahmy on 2021-03-24.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
--- InsulatorClassifier_v1/ViewController.swift
+++ InsulatorClassifier_v1/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var centerImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func cameraButtonTapped(_ sender: UIBarButtonItem) {
+        print("camera button tapped")
+    }
+    
 }
 
