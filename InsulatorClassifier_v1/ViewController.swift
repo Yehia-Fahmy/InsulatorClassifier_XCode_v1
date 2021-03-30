0dfc6facbe1bd44722a7c6ff9e32d64ad368bce8
Yehia <yehia.fahmy@uwaterloo.ca>
2021-03-30 11:28:38 -0400

    adding titile

--- InsulatorClassifier_v1/ViewController.swift
+++ InsulatorClassifier_v1/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationItem.title = "Insulator Classifier"
     }
 
     @IBAction func cameraButtonTapped(_ sender: UIBarButtonItem) {
